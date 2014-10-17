52ed0412d0041949542a4da5dafa21f0b436fb93
cube_zhu <cube_zhu@kingdee.com>
2014-10-17 15:34:02 +0800

    修改端口

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    listenPort: 3000,
+    listenPort: 3006,
     secret: 'cubechu',
     maxAge: 600000,
     db: 'mongodb://192.168.22.155:27017/myapp',
-  github: {
-	clientID: "6245ae3122580e682e90",
-	clientSecret: "186a34055da5184f9c6144063679b033642c0ab3",
-	callbackURL: "http://192.168.22.90:3000/auth/github/callback"
-	}
+    github: {
+        clientID: "6245ae3122580e682e90",
+        clientSecret: "186a34055da5184f9c6144063679b033642c0ab3",
+        callbackURL: "http://192.168.22.90:3000/auth/github/callback"
+    }
 }
