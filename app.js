0e8f686c8ecd244212f3a081e51e960a41abcb30
cubechu <cubechu@163.com>
2014-10-17 22:13:29 +0800

    update

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-// github oauth
+// github oauth第三方登录
 passport.use(new GithubStrategy(config.github, function (accessToken, refreshToken, profile, done) {
     done(null, profile);
 }));
